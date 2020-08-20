1b8bbb17da75cb33189909db161c328e946daea9
Stephen Neal <stephen.neal14@gmail.com>
2020-08-20 11:53:53 -0400

    Apply fixes from StyleCI (#1)

--- src/helpers.php
+++ src/helpers.php
@@ -1,26 +1,27 @@
 <?php
 
 /**
- * Reload current page
+ * Reload current page.
  *
  * @return string
  */
-function reloadWindow(): string {
+function reloadWindow(): string
+{
     return 'window.location.reload()';
 }
 
 /**
- * Return a JavaScript alert with a message
+ * Return a JavaScript alert with a message.
  * @param $msg
  * @return string
  */
-function jsAlert($msg): string {
-    return 'alert("' . $msg . '");';
+function jsAlert($msg): string
+{
+    return 'alert("'.$msg.'");';
 }
 
-
 /**
- * Open a $url in a new tab
+ * Open a $url in a new tab.
  *
  * @param string $url
  * @return string
@@ -30,13 +31,13 @@ function jsOpenInNewTab(string $url): string
     return "window.open('{$url}', '_blank');";
 }
 
-
 /**
- * Log a message to the client side Javascript console
+ * Log a message to the client side Javascript console.
  *
  * @param string $message
  * @return string
  */
-function consoleLog(string $message): string {
+function consoleLog(string $message): string
+{
     return "console.log('$message');";
 }
